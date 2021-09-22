b939c0185423aefd133da78fa505621a386a2c08
Donovan Cain Monteiro <88496063+don0716@users.noreply.github.com>
2021-09-23 01:20:22 +0530

    main

---
+++ app.js
@@ -0,0 +1,49 @@
+// input
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const errorMessage = document.querySelector("#error-message");
+const noOfNotes = document.querySelector(".no-of-notes")
+
+const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1]
+
+
+// checkButton.addEventListener("click", () => console.log("clicked")) 
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage()
+
+    if (billAmount.value > 0) {
+
+         if (cashGiven.value >= billAmount.value) {
+              const amountToBeReturned = cashGiven.value - billAmount.value;
+              calculateChange(amountToBeReturned);
+           } else {
+              showMessage("Cash Amount should be greater than Bill Amount")
+           }
+         
+
+    } else  {
+        showMessage("Amount should be greater than 0")
+    }
+      
+})
+
+// function
+function hideMessage() {
+    errorMessage.style.display = "none";
+}
+
+function calculateChange(amountToBeReturned) {
+    for (let i= 0; i < availableNotes.length; i++) {
+         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+         amountToBeReturned %= availableNotes[i];
+         noOfNotes[i].innerText = numberOfNotes;
+     }
+}
+
+function showMessage(msg) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = msg;
+}
+
