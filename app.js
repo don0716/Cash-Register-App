cbd3af355e4f8bcd78a58ede9d26741c76df72aa
Donovan Cain Monteiro <88496063+don0716@users.noreply.github.com>
2021-11-28 15:51:58 +0530

    show-cashGiven-after bill amount is entered

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes")
+const label = document.querySelectorAll(".input-label")
+// const nextBtn = document.querySelector("#next")
 
 const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1]
 
-// cashGiven.style.display = "none"
 
+hideContents()
 
 // checkButton.addEventListener("click", () => console.log("clicked")) 
 
@@ -16,6 +18,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage()
     
     if (billAmount.value > 0) {
+        showContents()
 
 
          if (cashGiven.value >= billAmount.value) {
@@ -39,6 +42,21 @@ function hideMessage() {
 function showMessage(msg) {
     errorMessage.style.display = "block";
     errorMessage.innerText = msg;
+}
+
+
+function hideContents(){
+    label[1].style.display = "none"
+    cashGiven.style.display = "none"
+    // checkButton.style.display = "none"
+}
+
+function showContents(){
+    cashGiven.style.display = "block";
+    cashGiven.style.margin = "auto";
+    label[1].style.display = "block"
+
+    // checkButton.style.display = "block"
 }
 
 function calculateChange(amountToBeReturned) {
